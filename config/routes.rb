c77bef2049e63c5d339778ea409da44f514c60f8
Sergio de Morais <srdemorais.sandbox@gmail.com>
2022-10-07 07:06:15 +0200

    update rails & ruby version

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  root to: "home#index"
-
-  get "about", to: "about#index"
-  resources :articles, only: [:show, :index]
-  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_08_034821) do
+ActiveRecord::Schema.define(version: 2022_10_07_050260) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
