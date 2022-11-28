92b4496d53e9edd0f6e913247a7bc6104cb23006
Sergio de Morais <srdemorais.sandbox@gmail.com>
2022-11-28 06:42:08 +0100

    add user signup form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "User was created successfully..."
+    else
+      render 'new'
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :articles
   validates :username, 
               presence: true, 
@@ -10,4 +11,5 @@ class User < ApplicationRecord
               uniqueness: { case_sensitive: true }, 
               length: { maximum: 105 },
               format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
