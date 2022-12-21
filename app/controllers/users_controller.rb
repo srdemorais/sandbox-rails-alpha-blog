b8472c4503ee26665f873c542892f13c4617935e
Sergio de Morais <srdemorais.sandbox@gmail.com>
2022-12-21 23:10:48 +0100

    add helpers to user authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update]
   
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Alpha Blog, #{@user.username}!"
       redirect_to articles_path
     else
@@ -35,6 +36,7 @@ class UsersController < ApplicationController
     end
   end
 
+  private
   def set_user
     @user = User.find(params[:id])
   end
