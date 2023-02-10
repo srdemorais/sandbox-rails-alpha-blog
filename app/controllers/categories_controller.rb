f822b44a75ff0a347f6018aba64c18300509fcd8
Sergio de Morais <srdemorais.sandbox@gmail.com>
2023-02-10 17:26:29 +0100

    edit categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,13 @@
 class CategoriesController < ApplicationController
 
   before_action :require_admin, except: [:index, :show]
+  before_action :set_category, only: [:show, :edit, :update]
 
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
   
   def show
-    @category = Category.find(params[:id])
     @articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -25,6 +25,18 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:notice] = "The category name was successfully updated..."
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def category_params
@@ -36,6 +48,10 @@ class CategoriesController < ApplicationController
       flash[:alert] = "Only admins can perform that action"
       redirect_to categories_path
     end
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
   end
 
 end
